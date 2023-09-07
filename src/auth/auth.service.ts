7b61a829a78dd5523dc529a3eeef6f744a7424fb
Ahmed Atef <atteff101@gmail.com>
2023-09-07 17:35:46 +0300

    update
    add db commands to packege scripts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, ForbiddenException } from '@nestjs/common';
-import { SignupDto } from './dtos';
+import { LoginDto, SignupDto } from './dtos';
 import * as argon from 'argon2';
 import { PrismaConnectService } from '../prisma-connect/prisma-connect.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
@@ -35,7 +35,26 @@ export class AuthService {
       }
     }
   }
-  signin() {
-    return { msg: 'signin' };
+  async signin(dto: LoginDto) {
+    // 1- find user with email
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    // 2- if user not found, throw exception
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+    // 3- compoare password with hashed password
+    const pwMatches = await argon.verify(user.password, dto.password);
+
+    // 4- if password incorrect, throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Password incorrect');
+    }
+
+    // 5- if all ok, login user
+    delete user.password;
+    return user;
   }
 }
